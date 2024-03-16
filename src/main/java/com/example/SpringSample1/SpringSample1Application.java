67a36d43810da3df04a8f54d14aa082c19fa5fac
Sureshpd89 <sureshpd89@gmail.com>
2024-03-17 00:28:55 +0530

    Initial commit

---
+++ src/main/java/com/example/SpringSample1/SpringSample1Application.java
@@ -0,0 +1,20 @@
+package com.example.SpringSample1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringSample1Application {
+	@GetMapping("/message")
+	String get(){
+		return "hello";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSample1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringSample1/SpringSample1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringSample1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSample1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
